ad8f4cddb70642b82927ff5ddd2beb9a06407f7d
CW4RR10R <arunptndpl@gmail.com>
2020-08-02 16:51:30 +0530

    Minified

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-from pyrogram import Client
+from pyrogram import Client, Filters
+import os, shutil
 from creds import my
-plugins = dict(
-        root="plugins"
-    )
-bot = Client(
+from telegraph import upload_file
+
+TGraph = Client(
     "Image upload bot",
     bot_token = my.BOT_TOKEN,
     api_id = my.API_ID,
-    api_hash = my.API_HASH,
-    plugins = plugins
+    api_hash = my.API_HASH
 )
-bot.run()
+
+
+@TGraph.on_message(Filters.command("start"))
+async def start(client, message):
+    await message.reply_text(f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by @W4RR10R", True)
+    
+@TGraph.on_message(Filters.photo)
+async def getimage(client, message):
+    tmp = os.path.join("downloads",str(message.chat.id))
+    if not os.path.isdir(tmp):
+        os.makedirs(tmp)
+    imgdir = tmp + "/" + str(message.message_id) +".jpg"
+    dwn = await message.reply_text("Downloading...", True)          
+    await client.download_media(
+            message=message,
+            file_name=imgdir
+        )
+    await dwn.edit_text("Uploading...")
+    try:
+        response = upload_file(imgdir)
+    except Exception as error:
+        await dwn.edit_text(f"Oops something went wrong\n{error}")
+        return
+    await dwn.edit_text(f"https://telegra.ph{response[0]}")
+    shutil.rmtree(tmp,ignore_errors=True)
+
+
+TGraph.run()
