cb0750279b4829358b4b6b0729a5c750a71cf70e
CW4RR10R <arunptndpl@gmail.com>
2020-11-09 14:02:51 +0530

    ...

--- creds.py
+++ creds.py
@@ -1,8 +1,10 @@
-from dotenv import load_dotenv, find_dotenv
 import os
+from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
-class my():
-    BOT_TOKEN = os.getenv("BOT_TOKEN")   # from @botfather
-    API_ID = int(os.getenv("API_ID"))    # from https://my.telegram.org/apps
-    API_HASH = os.getenv("API_HASH")     # from https://my.telegram.org/apps
+
+
+class Credentials:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")  # from @botfather
+    API_ID = int(os.getenv("API_ID"))  # from https://my.telegram.org/apps
+    API_HASH = os.getenv("API_HASH")  # from https://my.telegram.org/apps
--- main.py
+++ main.py
@@ -1,35 +1,37 @@
-from pyrogram import Client, filters
-import os, shutil
-from creds import my
-from telegraph import upload_file
+import os
+import shutil
 import logging
+from pyrogram import Client, filters
+from creds import Credentials
+from telegraph import upload_file
 
 logging.basicConfig(level=logging.INFO)
 
 
 TGraph = Client(
     "Image upload bot",
-    bot_token = my.BOT_TOKEN,
-    api_id = my.API_ID,
-    api_hash = my.API_HASH
+    bot_token=Credentials.BOT_TOKEN,
+    api_id=Credentials.API_ID,
+    api_hash=Credentials.API_HASH
 )
 
 
 @TGraph.on_message(filters.command("start"))
 async def start(client, message):
-    await message.reply_text(f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by @W4RR10R", True)
-    
+    await message.reply_text(
+        f"Hello {message.from_user.first_name},\nIm telegram to telegra.ph image uploader bot by @W4RR10R",
+        True,
+    )
+
+
 @TGraph.on_message(filters.photo)
 async def getimage(client, message):
-    tmp = os.path.join("downloads",str(message.chat.id))
+    tmp = os.path.join("downloads", str(message.chat.id))
     if not os.path.isdir(tmp):
         os.makedirs(tmp)
-    imgdir = tmp + "/" + str(message.message_id) +".jpg"
-    dwn = await message.reply_text("Downloading...", True)          
-    await client.download_media(
-            message=message,
-            file_name=imgdir
-        )
+    imgdir = tmp + "/" + str(message.message_id) + ".jpg"
+    dwn = await message.reply_text("Downloading...", True)
+    await client.download_media(message=message, file_name=imgdir)
     await dwn.edit_text("Uploading...")
     try:
         response = upload_file(imgdir)
@@ -37,7 +39,7 @@ async def getimage(client, message):
         await dwn.edit_text(f"Oops something went wrong\n{error}")
         return
     await dwn.edit_text(f"https://telegra.ph{response[0]}")
-    shutil.rmtree(tmp,ignore_errors=True)
+    shutil.rmtree(tmp, ignore_errors=True)
 
 
 TGraph.run()
