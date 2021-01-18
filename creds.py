95c948305fd907e9a3090bfb9b6dbd8a342f2a21
Abir Hasan <hackitabirhasan@gmail.com>
2021-01-19 04:27:22 +0600

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,10 +1,12 @@
 import os
-from dotenv import load_dotenv, find_dotenv
+#from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
+#load_dotenv(find_dotenv())
 
 
 class Credentials:
-    BOT_TOKEN = os.getenv("BOT_TOKEN")  # from @botfather
-    API_ID = int(os.getenv("API_ID"))  # from https://my.telegram.org/apps
-    API_HASH = os.getenv("API_HASH")  # from https://my.telegram.org/apps
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")  # from @botfather
+    API_ID = int(os.environ.get("API_ID"))  # from https://my.telegram.org/apps
+    API_HASH = os.environ.get("API_HASH")  # from https://my.telegram.org/apps
+
+# Okay 🤣
