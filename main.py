5b73c0c41f37a4e27f07f9d524bfae15669201a1
Abir Hasan <hackitabirhasan@gmail.com>
2021-01-19 13:06:24 +0600

    Update main.py

--- main.py
+++ main.py
@@ -137,11 +137,6 @@ async def getimage(client, message):
             )
             return
     ##
-    if message.document.file_name.lower().endwith("jpg", "png"):
-        pass
-    else:
-        return
-    
     tmp = os.path.join("downloads", str(message.chat.id))
     if not os.path.isdir(tmp):
         os.makedirs(tmp)
