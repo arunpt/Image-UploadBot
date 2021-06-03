972498237fba14e3f928c9f7233f8c0230d89c80
CW4RR10R <46273006+CW4RR10R@users.noreply.github.com>
2021-06-03 12:34:17 +0530

    even more simplified

--- main.py
+++ main.py
@@ -1,51 +1,57 @@
 import os
-import uuid
-import shutil
 import logging
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from creds import Credentials
 from telegraph import upload_file
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
 
 
 tgraph = Client(
     "Image upload bot",
     bot_token=Credentials.BOT_TOKEN,
     api_id=Credentials.API_ID,
-    api_hash=Credentials.API_HASH,
+    api_hash=Credentials.API_HASH
 )
 
 
 @tgraph.on_message(filters.command("start"))
 async def start(client, message):
     await message.reply_text(
-        text=f"Hello {message.from_user.mention},\nI'm A telegram to telegra.ph image uploader bot by @W4RR10R",
+        text=f"Hello {message.from_user.mention},\nI'm a telegram to telegra.ph image uploader bot by @W4RR10R",
         disable_web_page_preview=True
     )
 
 
 @tgraph.on_message(filters.photo)
 async def getimage(client, message):
-    tmp = os.path.join("downloads", str(message.chat.id))
-    if not os.path.isdir(tmp):
-        os.makedirs(tmp)
-    img_path = os.path.join(tmp, str(uuid.uuid4()) + ".jpg")
     dwn = await message.reply_text("Downloading to my server...", True)
-    img_path = await client.download_media(message=message, file_name=img_path)
+    img_path = await message.download()
     await dwn.edit_text("Uploading as telegra.ph link...")
     try:
-        response = upload_file(img_path)
+        url_path = upload_file(img_path)[0]
     except Exception as error:
         await dwn.edit_text(f"Oops something went wrong\n{error}")
         return
     await dwn.edit_text(
-        text=f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>",
+        text=f"<b>Link :-</b> <code>https://telegra.ph{url_path}</code>",
         disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Open Link", url=f"https://telegra.ph{response[0]}"), InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")]])
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="Open Link", url=f"https://telegra.ph{url_path}"
+                    ),
+                    InlineKeyboardButton(
+                        text="Share Link",
+                        url=f"https://telegram.me/share/url?url=https://telegra.ph{url_path}",
+                    )
+                ]
+            ]
+        )
     )
-    shutil.rmtree(tmp, ignore_errors=True)
+    os.remove(img_path)
 
 
 tgraph.run()
